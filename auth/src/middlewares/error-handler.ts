354d17ced6f3271d0be236f5436015dd7c723826
vikas6050 <vikas6050@gmail.com>
2024-01-17 10:22:51 +0530

    error handler update

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,6 @@
-import { Request, Response, NextFunction } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler = (
   err: Error,
@@ -9,18 +9,13 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map((error) => {
-      if (error.type === 'field') {
-        return { message: error.msg, field: error.path };
-      }
-    });
-    return res.status(400).send({ errors: formattedErrors });
+    return res.status(400).send({ errors: err.serializeErrors() });
   }
   if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({ errors: [{ message: err.reason }] });
+    return res.status(500).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({
-    errors: [{ message: 'Something went wrong' }],
+    errors: [{ message: "Something went wrong" }],
   });
 };
