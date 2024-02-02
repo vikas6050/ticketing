68b9f201f558e70250dea2208d0c03c4d375626b
vikas6050 <vikas6050@gmail.com>
2024-02-02 21:48:20 +0530

    231

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,13 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentuser }) => {
+  console.log(currentuser);
   return <h1>Landing Page</h1>;
 };
+
+// LandingPage.getInitialProps = async () => {
+//   const response = await axios.get('/api/users/currentuser')
+//   return response.data;
+// };
+
+export default LandingPage;
