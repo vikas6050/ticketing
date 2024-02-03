bc7b4e0d85950cebd4ac0da166915456a680e40a
vikas6050 <vikas6050@gmail.com>
2024-02-03 17:39:50 +0530

    sec 11 completed

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,19 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
-const LandingPage = ({ currentuser }) => {
-  console.log(currentuser);
-  return <h1>Landing Page</h1>;
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
-// LandingPage.getInitialProps = async () => {
-//   const response = await axios.get('/api/users/currentuser')
-//   return response.data;
-// };
+LandingPage.getInitialProps = async context => {
+  console.log('LANDING PAGE!');
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
 
 export default LandingPage;
