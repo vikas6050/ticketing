5b91477c65b02952d944454bc647dc887e039ddd
vikas6050 <vikas6050@gmail.com>
2024-02-08 12:41:48 +0530

    section 15

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,18 @@
+import { Message } from "node-nats-streaming";
+import { Listener } from "./base-listener";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
+  queueGroupName = "payments-service";
+
+  onMessage(data: any, msg: Message) {
+    console.log("Event data!", data);
+
+    console.log(data.name);
+    console.log(data.cost);
+
+    msg.ack();
+  }
+}
